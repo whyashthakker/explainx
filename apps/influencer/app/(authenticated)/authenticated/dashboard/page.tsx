7038fa42040bb960acbb1a9b5442aca7a7619f0f
WiseyAI <aryan@explainx.ai>
2024-12-20 16:12:12 +0530

    build : logged session

--- apps/influencer/app/(authenticated)/authenticated/dashboard/page.tsx
+++ apps/influencer/app/(authenticated)/authenticated/dashboard/page.tsx
@@ -1,5 +1,7 @@
 import React from "react";
-
+import { auth } from "../../../../auth";
 export default async function page() {
+  const session = await auth();
+  console.log(session);
   return <div>protected page</div>;
 }
