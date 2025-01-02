deb7ff40eeef40e1a872327082faf37cab90fe12
whyashthakker <nynansnh@gmail.com>
2025-01-02 19:35:24 +0530

    promise update for signup page

--- apps/brand/app/(auth)/signup/page.tsx
+++ apps/brand/app/(auth)/signup/page.tsx
@@ -1,28 +1,31 @@
-// app/signup/page.tsx
 "use client";
 
+import { use } from "react";
 import AuthForm from "../_components/AuthForm";
 import { handleGoogleSignIn, handleEmailSignIn } from "../_actions/actions";
 import { AuthSearchParams } from "../../../lib/types";
 
 interface PageProps {
-  params: {};
-  searchParams: AuthSearchParams;
+ params: Promise<{}>;
+ searchParams: Promise<AuthSearchParams>;
 }
 
-export default function SignupPage({ searchParams }: PageProps) {
-  return (
-    <AuthForm
-      mode="signup"
-      title="Sign up to Brand Portal"
-      description="Create your brand account and start collaborating with influencers"
-      handleEmailAuth={handleEmailSignIn}
-      handleGoogleAuth={handleGoogleSignIn}
-      searchParams={searchParams}
-      alternateAuthLink={{
-        text: "Already have an account? Sign in",
-        href: "/login",
-      }}
-    />
-  );
+export default function SignupPage(props: PageProps) {
+ const params = use(props.params);
+ const searchParams = use(props.searchParams);
+
+ return (
+   <AuthForm
+     mode="signup"
+     title="Sign up to Brand Portal"
+     description="Create your brand account and start collaborating with influencers"
+     handleEmailAuth={handleEmailSignIn}
+     handleGoogleAuth={handleGoogleSignIn}
+     searchParams={searchParams}
+     alternateAuthLink={{
+       text: "Already have an account? Sign in",
+       href: "/login", 
+     }}
+   />
+ );
 }
