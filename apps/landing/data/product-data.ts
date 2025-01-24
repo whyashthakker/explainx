c08f3413aa6dfcd757fd85eaec13f60d981c1d47
WiseyAI <aryan@explainx.ai>
2025-01-24 16:12:30 +0530

    chores : 24th jan blogs

--- apps/landing/data/product-data.ts
+++ apps/landing/data/product-data.ts
@@ -1,11 +1,11 @@
-// Generated on 2025-01-23T11:47:43.912Z
+// Generated on 2025-01-24T10:42:20.082Z
 
 const productData = [
   {
     "title": "AI Agent Development Services | explainx.ai",
     "file": "ai-agent-development",
     "description": "Professional AI agent development services to automate workflows, enhance customer experiences, and drive business efficiency. Custom solutions tailored to your needs.",
-    "datetime": "2025-01-23",
+    "datetime": "2025-01-24",
     "url": "https://www.explainx.ai/products/ai-agent-development"
   }
 ];
