cf50a57cb8e1e1ce4fd9b712384b954b6d08ea51
WiseyAI <aryan@explainx.ai>
2025-03-04 14:14:02 +0530

    build : rm accelerate

--- apps/web/auth.ts
+++ apps/web/auth.ts
@@ -1,7 +1,8 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import prisma from "@repo/db/client";
 import authConfig from "./auth.config";
+import { prisma } from "./lib/prisma";
+import { GetServerSidePropsContext } from "next";
 
 // declare module "@auth/core/adapters" {
 //   interface AdapterUser {
@@ -10,10 +11,27 @@ import authConfig from "./auth.config";
 //   }
 // }
 
-export const { handlers, auth, signIn, signOut } = NextAuth({
-  // session: {
-  //   strategy: "database",
-  // },
+// export const { handlers, auth, signIn, signOut } = NextAuth({
+//   session: {
+//     strategy: "jwt",
+//   },
+//   adapter: PrismaAdapter(prisma),
+//   ...authConfig,
+// });
+export const {
+  handlers,
+  auth,
+  signIn,
+  signOut,
+}: {
+  handlers: any;
+  auth: (context?: GetServerSidePropsContext) => Promise<any>;
+  signIn: (
+    provider: string,
+    options?: { redirectTo?: string; email?: string; state?: any },
+  ) => Promise<any>;
+  signOut: () => Promise<any>;
+} = NextAuth({
   adapter: PrismaAdapter(prisma),
   ...authConfig,
 });
