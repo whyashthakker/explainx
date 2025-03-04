bf86684b34bae864492bf0cb3d1d0180e3ad7759
WiseyAI <aryan@explainx.ai>
2025-03-04 12:00:26 +0530

    fix : types fix

--- apps/web/auth.ts
+++ apps/web/auth.ts
@@ -3,12 +3,12 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "@repo/db/client";
 import authConfig from "./auth.config";
 
-declare module "@auth/core/adapters" {
-  interface AdapterUser {
-    isAdmin: boolean;
-    isBetaTester: boolean;
-  }
-}
+// declare module "@auth/core/adapters" {
+//   interface AdapterUser {
+//     isAdmin: boolean;
+//     isBetaTester: boolean;
+//   }
+// }
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   // session: {
