b39207bc947039685d48b7359d529408bab3907e
rahulsan008 <rahul@explainx.ai>
2025-03-20 17:31:10 +0530

    change: terms and privacy policy

--- apps/landing/app/_components/terms-content.tsx
+++ apps/landing/app/_components/terms-content.tsx
@@ -3,10 +3,10 @@ import React from 'react';
 const TermsOfService: React.FC = () => {
 
     return (
-        <div className="flex justify-center items-center bg-white p-4">
-            <div className="max-w-6xl bg-white p-6 rounded-lg">
-                <h1 className="text-xl font-semibold text-center mb-4">Terms of Service</h1>
-                <p className="text-sm text-gray-600 mb-4">Last updated: December 27, 2024</p>
+        <div className="flex justify-center items-center bg-black p-4">
+            <div className="max-w-6xl bg-black p-6 rounded-lg mt-16">
+                <h1 className="text-xl font-semibold text-center mb-4 font-cal">Terms of Service</h1>
+                <p className="text-sm text-gray-300 mb-4">Last updated: December 27, 2024</p>
 
                 <div className="mb-6">
                     <p className="mb-4">Subject to these Terms of Service (this "Agreement"), ExplainX ("ExplainX", "we", "us", and/or "our") provides access to ExplainX's AI-powered influencer marketing platform (collectively, the "Services"). By using or accessing the Services, you acknowledge that you have read, understand, and agree to be bound by this Agreement.</p>
